74dd476745eff2b9912e9fe4e97a6a5fa699f303
Hitesh <HiteshLimbasiya1996>
2024-04-18 22:01:42 +0530

    first commit

---
+++ CSS/help.js
@@ -0,0 +1,11 @@
+function changeBg(){
+    var ast-main-header-wrap=document.getElementById('ast-main-header-wrap');
+    var scrollvalue=window.scrollY;
+    if(scrollvalue<150){
+        ast-main-header-wrap.classlist.remove('bgcolor');
+    }
+    else{
+        ast-main-header-wrap.classlist.add('bgcolor');
+    }
+    window.addEventListener('scroll',changeBg);
+}
